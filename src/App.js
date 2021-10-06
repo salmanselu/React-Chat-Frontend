5398ca3add6c57c658b99d0f5cc488c760c6a0ad
=Salman nazeer <salmanselu2002@gmail.com>
2021-10-06 19:40:38 +0530

    Added username support

--- src/App.js
+++ src/App.js
@@ -6,33 +6,57 @@ function App() {
 
   let [messageText, setMessageText] = useState("");
   let [messageList, setMessageList] = useState([]);
-  let baseUrl = "https://pethu-chat.herokuapp.com?message=";
+  let [username, setUsername] = useState(null);
+  let baseUrl = "https://pethu-chat.herokuapp.com";
 let textChange = (event) => {
   setMessageText(event.target.value);
 }
 let sendMessage = (event) => {
-  // event.preventDefault();
-  fetch(`${baseUrl}${messageText}`).then((response)=>{return response.json()}).then(data=>setMessageList(data.message))
+  event.preventDefault();
+  fetch(`${baseUrl}?message=${messageText}&username=${username}`).then((response)=>{return response.json()}).then(data=>setMessageList(data))
+  setMessageText("");
+}
+
+let usernameSetting = () => {
+  setUsername(messageText);
   setMessageText("");
 }
 
 setInterval(() => {
-  fetch(`${baseUrl}`).then(response =>{return response.json()}).then(data=>setMessageList(data.message));
+  fetch(`${baseUrl}`).then(response =>{return response.json()}).then(data=>setMessageList(data));
 }, 1000);
 
   return (
     <div className="App">
-      
+    { username !== null && 
+    <div>
       <div className="topspace">
-      <form className="send-box" onSubmit={sendMessage}>
-        <input onChange={ textChange } type="text" className="message-input" />
-        <input  className="send-button" type="submit"/>
-      </form>
+        <form className="send-box" onSubmit={sendMessage}>
+          <input onChange={ textChange } type="text" className="message-input" />
+          <input  className="send-button" type="submit"/>
+        </form>
       </div>
       <div className="response-box">
-        {messageList.map(message=>{return <div className="texts">{message}</div>})}
+        {messageList.map(message=>{
+          return  <div className="texts">
+           <div className="username">{message.username}:</div>
+           <div className="text">{message.message}</div>
+          </div>
+          })}
       </div>
-      
+    </div>
+    } 
+    { username === null && 
+    <>
+      <div className="topspace">
+        <h1 className="username-heading">Set Username</h1>
+        <form className="send-box" onSubmit={usernameSetting}>
+          <input onChange={ textChange } type="text" className="message-input" />
+          <input  className="send-button" type="submit"/>
+        </form>
+      </div>
+    </>
+    } 
     </div>
 
   );
